c5300beeba2c85869a5de4b396c5ea2012b0f960
toxydose <a.yakovlev911@gmail.com>
2017-11-27 22:27:36 +0200

    token tests

--- config_sample.py
+++ config_sample.py
@@ -2,3 +2,4 @@ AUTH_LOGIN = None  # specify your login to enable basic authorisation
 AUTH_PASS = None  # specify your pass to enable basic authorisation
 DB = 'mysql+pymysql://<username>:<password>@<host>/<dbname>?charset=utf8mb4'  # Строчка подключения к базе данных MySQL
 CASTLE = None # Флаг замка, который вы хотите обслуживать (например '🇮🇲'), оставьте None, чтобы не было ограничений
+APP_SECRET_KEY = 'secret'  # Ключ для сессии. Обязательно поменяйте
